5b1f77d7d1e043e74b56896a493fe40d5aa2d9a3
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-08-29 20:24:36 +0300

    Navbar component updated

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -33,12 +33,12 @@ export default function Navbar() {
               user ?
                 <>
 
-                  <li className='flex items-center'>
+                  {/* <li className='flex items-center'>
                     <NavLink to='profile' className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Profile</NavLink>
-                  </li>
+                  </li> */}
 
                   <li className='flex items-center'>
-                    <NavLink to='editProfile' className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Edit Profile</NavLink>
+                    <NavLink to='editProfile' className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"><i class="fa-solid fa-user-pen"></i> Edit Profile</NavLink>
                   </li>
 
                   <li>
